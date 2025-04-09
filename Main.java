89ad311e7c823159f4c3ac2074f3882720509012
JoaquinYato <164708509+JoaquinYato@users.noreply.github.com>
2025-04-09 17:32:19 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] arg){
-        int[] num={3,6,3,5,1,4,3,1,2};
-        int k=3;
-
-        Nummenor obj = new Nummenor(num, k);
-        System.out.println("El "+k+"° menor del arreglo es: "+obj.Menor());
-    }
-}
