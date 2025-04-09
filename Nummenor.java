0ba92f63a2b415627796443bcd8d7be836d63c55
JoaquinYato <164708509+JoaquinYato@users.noreply.github.com>
2025-04-09 17:53:51 -0500

    Update Nummenor.java

--- Nummenor.java
+++ Nummenor.java
@@ -1,6 +1,15 @@
 import java.util.Random;
 
 public class Nummenor {
+    public static void main(String[] arg){
+        int[] num={3,6,3,5,1,4,3,1,2};
+        int k=3;
+
+        Nummenor obj = new Nummenor(num, k);
+        System.out.println("El "+k+"° menor del arreglo es: "+obj.Menor());
+    }
+
+
     private int[] array;
     int k;
 
@@ -51,3 +60,5 @@ public class Nummenor {
         array[j] = x;
     }
 }
+
+
